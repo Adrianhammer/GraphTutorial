601449f556a5a4287a81c2962243b38872e8972d
Adrianhammer <adrian10hammer@gmail.com>
2025-08-28 13:06:21 +0200

    first commit

---
+++ GraphTutorial/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GraphTutorial;
+
+Console.WriteLine(".NET Graph Tutorial\n");
+
+var settings = Settings.LoadSettings();
+
+//Initialize Graph
+InitializeGraph(settings);
+
+await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose on of the following options: ");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display access token");
+    Console.WriteLine("2. List my inbox");
+    Console.WriteLine("3. Send mail");
+    Console.WriteLine("4. Make a graph call");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        //Set to invalid value
+        choice = -1;
+    }
+}
+
+switch (choice)
+{
+    case 0:
+        //exit program
+        Console.WriteLine("Goodbye...");
+        break;
+    case 1:
+        //display access token
+        await DisplayAccessTokenAsync();
+        break;
+    case 2:
+        //List emails from user's inbox
+        await ListInboxAsync();
+        break;
+    case 3:
+        //Send an email message
+        await SendMailAsync();
+        break;
+    case 4:
+        //Run any graph code
+        await MakeGraphCallAsync();
+        break;
+    default:
+        Console.WriteLine("Invalid choice! Please, try again.");
+        break;
+}
+
+
+void InitializeGraph(Settings settings)
+{
+    // TODO
+}
+
+async Task GreetUserAsync()
+{
+    // TODO
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    // TODO
+}
+
+async Task ListInboxAsync()
+{
+    // TODO
+}
+
+async Task SendMailAsync()
+{
+    // TODO
+}
+
+async Task MakeGraphCallAsync()
+{
+    // TODO
+}
---
+++ GraphTutorial/Settings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GraphTutorial;
+
+public class Settings
+{
+    public string? ClientId { get; set; }
+    public string? TenantId { get; set; }
+    public string[]? GraphUserScopes { get; set; }
+
+    public static Settings LoadSettings()
+    {
+        //Load settings
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            //Optional, will override appsettings.json
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            //Optional, will override both appsettings if used
+            .AddUserSecrets<Program>()
+            .Build();
+        
+        return config.GetRequiredSection("Settings").Get<Settings>() ??
+               throw new Exception("Could not load app settings. See README for configuration instructions.");
+    }
+}
